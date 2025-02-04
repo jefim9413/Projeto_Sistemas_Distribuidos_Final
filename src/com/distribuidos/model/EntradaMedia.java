0e2ab4f3d98353f2ad70245d58eeb6f8843b5e43
Jefferson Marques <56725964+jefim9413@users.noreply.github.com>
2025-02-03 22:47:27 -0300

    entrada media

---
+++ src/com/distribuidos/model/EntradaMedia.java
@@ -0,0 +1,16 @@
+
+package com.distribuidos.model;
+
+import java.util.List;
+
+public class EntradaMedia {
+    private List<Integer> numeros;
+
+    public EntradaMedia(List<Integer> numeros) {
+        this.numeros = numeros;
+    }
+
+    public List<Integer> getNumeros() {
+        return numeros;
+    }
+}
