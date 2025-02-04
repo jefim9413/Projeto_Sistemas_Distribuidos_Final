8c602b1519fc7aedc5c9b166a29328b28aa43178
Jefferson Marques <56725964+jefim9413@users.noreply.github.com>
2025-02-03 22:48:39 -0300

    saida media

---
+++ src/com/distribuidos/model/SaidaMedia.java
@@ -0,0 +1,14 @@
+
+package com.distribuidos.model;
+
+public class SaidaMedia {
+    private double resultado;
+
+    public SaidaMedia(double resultado) {
+        this.resultado = resultado;
+    }
+
+    public double getResultado() {
+        return resultado;
+    }
+}
