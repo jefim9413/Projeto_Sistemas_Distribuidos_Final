097973dae2ac27ca97cd38ba86b3ecb6a0b1c097
Jefferson Marques <56725964+jefim9413@users.noreply.github.com>
2025-02-03 22:48:46 -0300

    saida equacao

---
+++ src/com/distribuidos/model/SaidaEquacao.java
@@ -0,0 +1,14 @@
+
+package com.distribuidos.model;
+
+public class SaidaEquacao {
+    private String resultado;
+
+    public SaidaEquacao(String resultado) {
+        this.resultado = resultado;
+    }
+
+    public String getResultado() {
+        return resultado;
+    }
+}
