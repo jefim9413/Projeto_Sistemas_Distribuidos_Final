59316355ac6db1389837b3c19f5a493510a38130
Jefferson Marques <56725964+jefim9413@users.noreply.github.com>
2025-02-03 22:48:31 -0300

    saida soma

---
+++ src/com/distribuidos/model/SaidaSoma.java
@@ -0,0 +1,14 @@
+
+package com.distribuidos.model;
+
+public class SaidaSoma {
+    private int resultado;
+
+    public SaidaSoma(int resultado) {
+        this.resultado = resultado;
+    }
+
+    public int getResultado() {
+        return resultado;
+    }
+}
