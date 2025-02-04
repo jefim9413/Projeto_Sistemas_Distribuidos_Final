986e28748302d71f3ee0e9c59acfee96284d899b
Jefferson Marques <56725964+jefim9413@users.noreply.github.com>
2025-02-03 22:47:35 -0300

    entrada soma

---
+++ src/com/distribuidos/model/EntradaSoma.java
@@ -0,0 +1,16 @@
+
+package com.distribuidos.model;
+
+import java.util.List;
+
+public class EntradaSoma {
+    private List<Integer> numeros;
+
+    public EntradaSoma(List<Integer> numeros) {
+        this.numeros = numeros;
+    }
+
+    public List<Integer> getNumeros() {
+        return numeros;
+    }
+}
