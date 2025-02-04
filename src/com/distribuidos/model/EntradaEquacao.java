328e086781307775522ac01a1487075983a31429
Jefferson Marques <56725964+jefim9413@users.noreply.github.com>
2025-02-03 22:47:19 -0300

    entradaEquacao

---
+++ src/com/distribuidos/model/EntradaEquacao.java
@@ -0,0 +1,16 @@
+
+package com.distribuidos.model;
+
+public class EntradaEquacao {
+    private int a, b, c;
+
+    public EntradaEquacao(int a, int b, int c) {
+        this.a = a;
+        this.b = b;
+        this.c = c;
+    }
+
+    public int getA() { return a; }
+    public int getB() { return b; }
+    public int getC() { return c; }
+}
